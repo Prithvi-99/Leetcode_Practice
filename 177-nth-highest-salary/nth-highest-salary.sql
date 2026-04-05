7f3615377f784fa0857b720e4c4a0ca795ef2985
Prithvi Narayan Singh <prithviofficial99@gmail.com>
2026-04-05 23:39:08 +0530

    Time: 863 ms (6.28%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,10 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-    select DISTINCT(e1.salary)
+    select distinct(salary)
     from Employee e1
-    where N-1 = (Select count(DISTINCT(e2.salary))
+    where N-1 = (select count(distinct(salary))
                 from Employee e2
                 where e2.salary>e1.salary)
+
   );
 END
