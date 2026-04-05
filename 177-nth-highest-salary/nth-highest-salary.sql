28565f0073eb565fa1d318c63843983e176e426e
Prithvi Narayan Singh <prithviofficial99@gmail.com>
2026-04-05 23:39:42 +0530

    Time: 848 ms (6.74%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -3,7 +3,7 @@ BEGIN
   RETURN (
     select distinct(salary)
     from Employee e1
-    where N-1 = (select count(distinct(salary))
+    where N-1 = (select count(distinct(e2.salary))
                 from Employee e2
                 where e1.salary<e2.salary)
 
